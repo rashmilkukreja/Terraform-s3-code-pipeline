29ce8ad11cb981b40eb12814a6bc441a38def436
Rashmil Kukreja <rashmil.kukreja@gmail.com>
2026-03-09 16:03:42 +0530

    terraform

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "my_bucket_region" {
+    description = "my default bucket region"
+    type = string
+    default = "eu-north-1"
+}
+
+variable "my_bucket_name" {
+    description = "my bucket name"
+    type = string
+    default = "rashmilkukreja-cv"
+}
